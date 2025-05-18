33a27257b03c078809d73b3eb0ae8e30f5688035
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2025-05-18 23:23:54 +0530

    feat: added validations in UserDTO for improved clarity and user feedback during registration process

--- src/main/java/com/dipanshushukla/realtimechatappauthservice/dto/UserDTO.java
+++ src/main/java/com/dipanshushukla/realtimechatappauthservice/dto/UserDTO.java
@@ -5,6 +5,8 @@ import com.dipanshushukla.realtimechatappauthservice.model.Role;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
@@ -12,20 +14,24 @@ import lombok.Data;
 @Builder
 public class UserDTO {
 
-    @NotNull
-    @NotBlank
+    @NotNull(message = "fullName must not be null")
+    @NotBlank(message = "fullName must not be blank")
+    @Size(min = 2, message = "fullName must be at least 2 characters long")
     private String fullName;
 
-    @NotNull
-    @NotBlank
+    @NotNull(message = "username must not be null")
+    @NotBlank(message = "username must not be blank")
+    @Pattern(regexp = "^[A-Za-z0-9_]{3,32}$", message = "username must be 3–32 characters, contain only letters or digits, and only be seperated by underscore (_)")
     private String username;
 
-    @NotNull
-    @NotBlank
+    @NotNull(message = "password must not be null")
+    @NotBlank(message = "password must not be blank")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9])\\S{8,64}$", message = "password must be 8–64 characters, contain at least one uppercase letter, one lowercase letter, one digit, and one special character")
     private String password;
 
-    @NotNull
-    @NotBlank
+    @NotNull(message = "email must not be null")
+    @NotBlank(message = "email must not be blank")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$", message = "Invalid email address")
     private String email;
 
     private Role role;
