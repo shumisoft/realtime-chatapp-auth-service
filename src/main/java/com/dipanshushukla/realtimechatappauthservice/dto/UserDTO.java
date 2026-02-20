2a1e9b084863c1f999c486fb32b631a2b5960f85
Dipanshu Shukla <shuklascientist5@gmail.com>
2026-02-20 12:07:46 +0530

    chore: upgrade springboot ver to 4.0.3

--- src/main/java/com/dipanshushukla/realtimechatappauthservice/dto/UserDTO.java
+++ src/main/java/com/dipanshushukla/realtimechatappauthservice/dto/UserDTO.java
@@ -7,11 +7,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
 
     @NotNull(message = "fullName must not be null")
